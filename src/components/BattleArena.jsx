4e3beee29a387cd8de9d53b816574d1845022dab
Arnau-Mas <arnau.mava@gmail.com>
2022-08-05 19:34:35 +0200

    setup design

---
+++ src/components/BattleArena.jsx
@@ -0,0 +1,10 @@
+import "./BattleArena.css"
+import logo from "../assets/pokemonLogo.png"
+export function BattleArena(){
+    return (
+        <div className="battleArena">
+            <img src={logo} alt=""/>
+            <p></p>
+        </div>
+    )
+}
---
+++ src/components/PokemonSetup.jsx
@@ -0,0 +1,10 @@
+import './PokemonSetup.css'
+import { BattleArena } from './BattleArena'
+
+export function PokemonSetup(){
+    return (
+        <div className='pokemonSetup'>
+            <BattleArena/>
+        </div>
+    )
+}
