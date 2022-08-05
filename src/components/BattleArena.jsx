00fa7bb228647bd7df7e55eb26b4937bdf3dc4cd
Arnau-Mas-ASUS <arnau.mava@gmail.com>
2022-08-05 23:13:13 +0200

    updating

--- src/components/BattleArena.jsx
+++ src/components/BattleArena.jsx
@@ -6,20 +6,33 @@ import { useEffect, useState } from "react"
 
 export function BattleArena(){
     const [pokeList, setPokeList] = useState([])
+    const [pokemon1, setPokemon1] = useState({});
+    const [pokemon2, setPokemon2] = useState({});
 
     useEffect(() => {
         fetch("https://pokeapi.co/api/v2/pokemon/")
         .then(res => res.json())
-        .then(data => setPokeList(data.results))
+        .then(data => {
+            setPokeList(data.results)
+        })
     }, [])
-    
+
+    useEffect(() => {
+        fetch("https://pokeapi.co/api/v2/pokemon/bulbasaur")
+        .then(res => res.json())
+        .then(data => {
+            setPokemon1(data)
+            setPokemon2(data)
+        })
+    }, [])
+
     return (
         <div className="battleArena">
             <img src={logo} alt=""/>
             <div style={{width:"100%", height:"100%", display:"flex", position:"absolute"}}>
-                <PokemonSelect list={pokeList} />
-                <img style={{position:"relative", height:"7rem", top:"14rem", left:"1rem"}} src={vsLogo}/>
-                <PokemonSelect list={pokeList}/>
+                <PokemonSelect list={pokeList} pokemon={pokemon1} setPokemon={setPokemon1} />
+                <img style={{position:"relative", height:"7rem", margin:"auto"}} src={vsLogo}/>
+                <PokemonSelect list={pokeList} pokemon={pokemon2} setPokemon={setPokemon2}/>
             </div>
         </div>
     )
--- src/components/PokemonSelect.jsx
+++ src/components/PokemonSelect.jsx
@@ -1,12 +1,28 @@
+import { useEffect, useState } from "react";
 import "./PokemonSelect.css";
 
-export function PokemonSelect({list}){
-    console.log(list)
+export function PokemonSelect({list, pokemon, setPokemon}){
+     const [chosenPokemon, setChosenPokemon] = useState("");
+     const [photo, setPhoto] = useState("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/1.png"); 
+    function setChoice(e){
+        let objectPokemon = list.find(pokemon => pokemon.name === e.target.value);
+        setChosenPokemon(objectPokemon);
+    }
+
+    useEffect(() => {
+        fetch(chosenPokemon.url)
+        .then(res => res.json())
+        .then(data => {
+            setPokemon(data)
+            setPhoto(data.sprites.other.home.front_default)})
+    }, [chosenPokemon])
+    
     return (
         <div className="pokemonSelect">
+            {Object.entries(pokemon) && <img style={{height:"10rem"}} src={photo} />}
             <p className="pokemonSelect__p">Choose a Pokemon</p>
-            <select>
-                {list.map(pokemon => <option key={pokemon.name}>{pokemon.name}</option>)}
+            <select value={chosenPokemon.name} onChange={setChoice}>
+                {list.map(pokemon => <option value={pokemon.name} key={pokemon.name}>{pokemon.name}</option>)}
             </select>
         </div>
     )
