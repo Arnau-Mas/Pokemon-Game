7c6518238877d584a83bd98c0bbe6cbf0e5a69d7
Arnau-Mas <arnau.mava@gmail.com>
2022-08-07 16:29:30 +0200

    update

--- src/components/battle/PokemonBattle.jsx
+++ src/components/battle/PokemonBattle.jsx
@@ -3,11 +3,12 @@ import "./PokemonBattle.css"
 import { PokemonFighter } from "./PokemonFighter"
 
 export function PokemonBattle({pokemon1, pokemon2}){
-    let life1 = pokemon1.stats[0].base_stat;
-    let life2 = pokemon2.stats[0].base_stat
-    const [fighter1, setFighter1] = useState(pokemon1);
-    const [fighter2, setFighter2] = useState(pokemon2);
-    const [hideDamage, setHideDamage] = useState(true)
+    const lifePokemon1 = pokemon1.stats[0].base_stat;
+    const lifePokemon2 = pokemon2.stats[0].base_stat;
+    const [life1, setLife1] = useState(pokemon1.stats[0].base_stat)
+    const [life2, setLife2] = useState(pokemon2.stats[0].base_stat)
+    const [hideDamage, setHideDamage] = useState(true);
+    const [damage, setDamage] = useState("");
     const [turn, setTurn] = useState(1)
     
     useEffect(() => {
@@ -17,15 +18,24 @@ export function PokemonBattle({pokemon1, pokemon2}){
         }, 1000)
         return () => clearTimeout(timer)
     }, [turn])
-    
+
     return (
-        <div style={{width:"100%", height:"100%", display:"flex", position:"absolute"}}>
-            <PokemonFighter turn={turn===1 ? true : false} setTurn={setTurn} pokemon={fighter1} face={false} life={life1} />
-                <div style={{position:"relative", fontSize:"24px", top:"10rem"}}>
+            <div style={{width:"100%", height:"100%", display:"flex", position:"absolute"}}>
+            <PokemonFighter turn={turn===1 && life1>0 && life2>0 ? true : false} setTurn={setTurn} barPercent={life1*100/lifePokemon1 < 0 ? 0 : life1*100/lifePokemon1} pokemon={pokemon1} setLife={setLife2} face={false} life={life1} setDamage={setDamage} />
+            {
+                life1>0 && life2>0 ? 
+                    <div style={{position:"relative", fontSize:"24px", top:"10rem"}}>
                     <p>Damage:</p>
-                    <p style={{color:"red", textAlign:"center"}} hidden={hideDamage}>a</p>
+                    {damage && <p style={{color:"red", textAlign:"center", marginTop:"1rem", fontSize:"2rem"}} hidden={hideDamage}>-{damage}</p>}
+                    </div>
+                :
+                <div style={{position:"relative", fontSize:"24px", top:"10rem", height:"10rem", width:"9rem"}}>
+                    <p>The Winner</p>
+                   <p style={{backgroundColor:"green", textAlign:"center", color:"white"}}>{life1<=0 ? "🢂" : "🢀"}</p>
                 </div>
-            <PokemonFighter turn={turn===2 ? true : false} setTurn={setTurn} pokemon={fighter2} face={true} life={life2} />
-        </div>
+
+            }
+            <PokemonFighter turn={turn===2 && life1>0 && life2>0 ? true : false} setTurn={setTurn} barPercent={life2*100/lifePokemon2 < 0 ? 0 : life2*100/lifePokemon2} pokemon={pokemon2} setLife={setLife1} face={true} life={life2} setDamage={setDamage} />
+            </div>
     )
 }
--- src/components/battle/PokemonFighter.jsx
+++ src/components/battle/PokemonFighter.jsx
@@ -1,29 +1,38 @@
 import { useState, useEffect } from "react"
 
-export function PokemonFighter({pokemon, face, life, turn, setTurn}){
+export function PokemonFighter({pokemon, face, life, turn, setTurn, setDamage, setLife, barPercent}){
+    console.log(pokemon.name, barPercent)
     const [color, setColor] = useState("rgba(66, 255, 66, 0.699)");
     const moves = pokemon.moves;
+
+    function attack(number){
+        setLife(prev => prev-number);
+        setDamage(number);
+        setTurn(prev => prev===1 ? 2 : 1)
+    }
+
     useEffect(() => {
         if(life<=pokemon.stats[0].base_stat/2 && life>pokemon.stats[0].base_stat/3){
             setColor("orange")
-        }else if(life<=pokemon.stats[0].base_stat/3){
+        }else if(life<=pokemon.stats[0].base_stat/4){
             setColor("red")
         }else{
             setColor("rgba(66, 255, 66, 0.699)")
         }
-    }, [pokemon])
+    }, [life])
+
     return (
     <div className="pokemonContainer">
         <div className="hpBarContainer">
-            <div className="hpBar" style={{width:"100%", backgroundColor:color}}>
+            <div className="hpBar" style={{width:`${barPercent}%`, backgroundColor:color}}>
             </div>
         </div>
         <img style={{height: turn ? "12rem" : "10rem", filter: !turn && 'grayscale(100%)'}} src={face ? pokemon.sprites.front_default : pokemon.sprites.back_default}/>
         <div style={{width:"70%", display:"grid", gridTemplateColumns:"2", gap:"0.5rem", justifyContent:"center", marginBottom:"1rem", filter: !turn && 'grayscale(100%)'}}>
-            <button disabled={!turn} className="moveContainer__move" onClick={() => setTurn(prev => prev===1 ? 2 : 1)} style={{gridColumn:"1/2"}}>{moves[0].move.name}</button>
-            <button disabled={!turn} className="moveContainer__move" onClick={() => setTurn(prev => prev===1 ? 2 : 1)} style={{gridColumn:"2/2"}}>{moves[1].move.name}</button>
-            <button disabled={!turn} className="moveContainer__move" onClick={() => setTurn(prev => prev===1 ? 2 : 1)} style={{gridColumn:"1/2"}}>{moves[2].move.name}</button>
-            <button disabled={!turn} className="moveContainer__move" onClick={() => setTurn(prev => prev===1 ? 2 : 1)} style={{gridColumn:"2/2"}}>{moves[3].move.name}</button>
+            <button disabled={!turn} className="moveContainer__move" onClick={() => attack(10)} style={{gridColumn:"1/2"}}>{moves[0].move.name}</button>
+            <button disabled={!turn} className="moveContainer__move" onClick={() => attack(15)} style={{gridColumn:"2/2"}}>{moves[1].move.name}</button>
+            <button disabled={!turn} className="moveContainer__move" onClick={() => attack(20)} style={{gridColumn:"1/2"}}>{moves[2].move.name}</button>
+            <button disabled={!turn} className="moveContainer__move" onClick={() => attack(5)} style={{gridColumn:"2/2"}}>{moves[3].move.name}</button>
         </div>
     </div>
     )
