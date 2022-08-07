a5946b1ccc963a5f72b03a1942ebcad3c7373651
Arnau-Mas-ASUS <arnau.mava@gmail.com>
2022-08-07 15:37:04 +0200

    damage message

--- src/components/battle/PokemonBattle.jsx
+++ src/components/battle/PokemonBattle.jsx
@@ -7,21 +7,24 @@ export function PokemonBattle({pokemon1, pokemon2}){
     let life2 = pokemon2.stats[0].base_stat
     const [fighter1, setFighter1] = useState(pokemon1);
     const [fighter2, setFighter2] = useState(pokemon2);
-    const [showDamage, setShowDamage] = useState(false)
+    const [hideDamage, setHideDamage] = useState(true)
     const [turn, setTurn] = useState(1)
-/*     
+    
     useEffect(() => {
-        setShowDamage(true);
+        setHideDamage(false);
         const timer = setTimeout(() => {
-            setShowDamage(false)
-        }, 2000)
+            setHideDamage(true)
+        }, 1000)
         return () => clearTimeout(timer)
     }, [turn])
-     */
+    
     return (
         <div style={{width:"100%", height:"100%", display:"flex", position:"absolute"}}>
             <PokemonFighter turn={turn===1 ? true : false} setTurn={setTurn} pokemon={fighter1} face={false} life={life1} />
-                <p style={{position:"relative", fontSize:"24px", top:"10rem", color:"red", hidden:showDamage}}>a</p>
+                <div style={{position:"relative", fontSize:"24px", top:"10rem"}}>
+                    <p>Damage:</p>
+                    <p style={{color:"red", textAlign:"center"}} hidden={hideDamage}>a</p>
+                </div>
             <PokemonFighter turn={turn===2 ? true : false} setTurn={setTurn} pokemon={fighter2} face={true} life={life2} />
         </div>
     )
