f189128544b815bd1fefbf4a2727cb4c043d7279
Arnau-Mas-ASUS <arnau.mava@gmail.com>
2022-08-07 11:07:15 +0200

    movements panel

--- src/components/PokemonBattle.jsx
+++ src/components/PokemonBattle.jsx
@@ -5,18 +5,18 @@ export function PokemonBattle({pokemon1, pokemon2, list}){
     return (
         <div style={{width:"100%", height:"100%", display:"flex", position:"absolute"}}>
             <div className="pokemonContainer">
-                <img style={{height:"15rem"}} src={pokemon1.sprites.back_default}/>
                 <p>{pokemon1.name}</p>
-                <div style={{backgroundColor:"green", width:"70%", display:"grid", gridTemplateColumns:"2", gap:"1rem", justifyContent:"center"}}>
-                    <div style={{gridColumn:"1/2", backgroundColor:"yellow"}}>attack1</div>
-                    <div style={{gridColumn:"2/2", backgroundColor:"yellow"}}>attack2</div>
-                    <div style={{gridColumn:"1/2", backgroundColor:"yellow"}}>attack3</div>
-                    <div style={{gridColumn:"2/2", backgroundColor:"yellow"}}>attack4</div>
+                <img style={{height:"12rem"}} src={pokemon1.sprites.back_default}/>
+                <div style={{width:"70%", display:"grid", gridTemplateColumns:"2", gap:"0.5rem", justifyContent:"center", marginBottom:"1rem"}}>
+                    <button className="moveContainer__move" style={{gridColumn:"1/2"}}>attack1</button>
+                    <button className="moveContainer__move" style={{gridColumn:"2/2"}}>attack2</button>
+                    <button className="moveContainer__move" style={{gridColumn:"1/2"}}>attack3</button>
+                    <button className="moveContainer__move" style={{gridColumn:"2/2"}}>attack4</button>
                 </div>
             </div>
             <div className="pokemonContainer">
-                <img style={{height:"15rem"}} src={pokemon2.sprites.front_default}/>
                 <p>{pokemon1.name}</p>
+                <img style={{height:"12rem"}} src={pokemon2.sprites.front_default}/>
             </div>
         </div>
     )
