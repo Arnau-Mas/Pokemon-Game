a2ae5d92169d158076cfd0bf4f9af324ef2d97af
Arnau-Mas <arnau.mava@gmail.com>
2022-08-07 12:52:10 +0200

    setting moves panel

--- src/components/battle/PokemonBattle.jsx
+++ src/components/battle/PokemonBattle.jsx
@@ -3,12 +3,14 @@ import "./PokemonBattle.css"
 import { PokemonFighter } from "./PokemonFighter"
 
 export function PokemonBattle({pokemon1, pokemon2}){
+    let life1 = pokemon1.stats[0].base_stat;
+    let life2 = pokemon2.stats[0].base_stat
     const [fighter1, setFighter1] = useState(pokemon1);
     const [fighter2, setFighter2] = useState(pokemon2);
     return (
         <div style={{width:"100%", height:"100%", display:"flex", position:"absolute"}}>
-            <PokemonFighter pokemon={fighter1} face={false} />
-            <PokemonFighter pokemon={fighter2} face={true} />
+            <PokemonFighter pokemon={fighter1} face={false} life={life1} />
+            <PokemonFighter pokemon={fighter2} face={true} life={life2} />
         </div>
     )
 }
--- src/components/battle/PokemonFighter.jsx
+++ src/components/battle/PokemonFighter.jsx
@@ -1,19 +1,31 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
-export function PokemonFighter({pokemon, face}){
+export function PokemonFighter({pokemon, face, life}){
+    const [color, setColor] = useState("rgba(66, 255, 66, 0.699)");
+    const moves = pokemon.moves;
+    console.log(moves)
+    useEffect(() => {
+        if(life<=pokemon.stats[0].base_stat/2 && life>pokemon.stats[0].base_stat/3){
+            setColor("orange")
+        }else if(life<=pokemon.stats[0].base_stat/3){
+            setColor("red")
+        }else{
+            setColor("rgba(66, 255, 66, 0.699)")
+        }
+    }, [pokemon])
 
     return (
     <div className="pokemonContainer">
         <div className="hpBarContainer">
-            <div className="hpBar" style={{width:"100%", backgroundColor:"rgba(66, 255, 66, 0.699)"}}>
+            <div className="hpBar" style={{width:"100%", backgroundColor:color}}>
             </div>
         </div>
         <img style={{height:"12rem"}} src={face ? pokemon.sprites.front_default : pokemon.sprites.back_default}/>
         <div style={{width:"70%", display:"grid", gridTemplateColumns:"2", gap:"0.5rem", justifyContent:"center", marginBottom:"1rem"}}>
-            <button className="moveContainer__move" style={{gridColumn:"1/2"}}>attack1</button>
-            <button className="moveContainer__move" style={{gridColumn:"2/2"}}>attack2</button>
-            <button className="moveContainer__move" style={{gridColumn:"1/2"}}>attack3</button>
-            <button className="moveContainer__move" style={{gridColumn:"2/2"}}>attack4</button>
+            <button className="moveContainer__move" style={{gridColumn:"1/2"}}>{moves[0].move.name}</button>
+            <button className="moveContainer__move" style={{gridColumn:"2/2"}}>{moves[1].move.name}</button>
+            <button className="moveContainer__move" style={{gridColumn:"1/2"}}>{moves[2].move.name}</button>
+            <button className="moveContainer__move" style={{gridColumn:"2/2"}}>{moves[3].move.name}</button>
         </div>
     </div>
     )
